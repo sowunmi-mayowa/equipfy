31be86733f0df0898ecba329a2b0c09f2dba156f
sowunmi mayowa <smayowa176@gmail.com>
2026-02-07 02:23:01 +0100

    updated vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,20 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
   return {
+    resolve: {
+      dedupe: ["react", "react-dom"],
+    },
     define: {
-      'process.env.REACT_APP_BACKEND_URL': JSON.stringify(env.REACT_APP_BACKEND_URL)
+      "process.env.REACT_APP_BACKEND_URL": JSON.stringify(
+        env.REACT_APP_BACKEND_URL,
+      ),
+    },
+    optimizeDeps: {
+      include: ["react", "react-dom"],
     },
     plugins: [react()],
     build: {
@@ -15,15 +23,17 @@ export default defineConfig(({ mode }) => {
       rollupOptions: {
         output: {
           manualChunks(id) {
-            if (id.includes('node_modules')) {
-              if (id.includes('react') || id.includes('react-dom')) return 'vendor_react';
-              if (id.includes('react-router-dom')) return 'vendor_router';
-              if (id.includes('react-slick') || id.includes('slick-carousel')) return 'vendor_slick';
-              return 'vendor';
+            if (id.includes("node_modules")) {
+              if (id.includes("react") || id.includes("react-dom"))
+                return "vendor_react";
+              if (id.includes("react-router-dom")) return "vendor_router";
+              if (id.includes("react-slick") || id.includes("slick-carousel"))
+                return "vendor_slick";
+              return "vendor";
             }
-          }
-        }
-      }
-    }
-  }
-})
+          },
+        },
+      },
+    },
+  };
+});
